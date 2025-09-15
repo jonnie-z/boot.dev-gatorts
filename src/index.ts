22529c915fd79073f5c19239253c7da3749efabb
jonnie-z <jonnie@jonniez.com>
2025-09-15 17:55:56 -0400

    Totally done, right?

---
+++ src/index.ts
@@ -0,0 +1,69 @@
+import {
+  CommandsRegistry,
+  registerCommand,
+  runCommand,
+} from "./commands/commands";
+import {
+  handlerListUsers,
+  handlerLogin,
+  handlerRegister,
+} from "./commands/users";
+import { handlerReset } from "./commands/reset";
+import { handlerAgg } from "./commands/aggregate";
+import { handlerAddFeed, handlerListFeeds } from "./commands/feeds";
+import { handlerFollow, handlerListFeedFollows, handlerUnfollow } from "./commands/feed-follows";
+import { middlewareLoggedIn } from "./middleware";
+
+async function main() {
+  const args = process.argv.slice(2);
+
+  if (args.length < 1) {
+    console.log("usage: cli <command> [args...]");
+    process.exit(1);
+  }
+
+  const cmdName = args[0];
+  const cmdArgs = args.slice(1);
+  const commandsRegistry: CommandsRegistry = {};
+
+  registerCommand(commandsRegistry, "login", handlerLogin);
+  registerCommand(commandsRegistry, "register", handlerRegister);
+  registerCommand(commandsRegistry, "reset", handlerReset);
+  registerCommand(commandsRegistry, "users", handlerListUsers);
+  registerCommand(commandsRegistry, "agg", handlerAgg);
+  registerCommand(
+    commandsRegistry,
+    "addfeed",
+    middlewareLoggedIn(handlerAddFeed),
+  );
+  registerCommand(commandsRegistry, "feeds", handlerListFeeds);
+  registerCommand(
+    commandsRegistry,
+    "follow",
+    middlewareLoggedIn(handlerFollow),
+  );
+  registerCommand(
+    commandsRegistry,
+    "unfollow",
+    middlewareLoggedIn(handlerUnfollow),
+  );
+  registerCommand(
+    commandsRegistry,
+    "following",
+    middlewareLoggedIn(handlerListFeedFollows),
+  );
+
+  try {
+    await runCommand(commandsRegistry, cmdName, ...cmdArgs);
+  } catch (err) {
+    if (err instanceof Error) {
+      console.error(`Error running command ${cmdName}: ${err.message}`);
+    } else {
+      console.error(`Error running command ${cmdName}: ${err}`);
+    }
+    process.exit(1);
+  }
+  process.exit(0);
+}
+
+main();
---
+++ src/lib/db/queries/feeds.ts
@@ -0,0 +1,31 @@
+import { eq } from "drizzle-orm";
+import { db } from "..";
+import { feeds } from "../schema";
+import { firstOrUndefined } from "./utils";
+
+export async function createFeed(
+  feedName: string,
+  url: string,
+  userId: string,
+) {
+  const result = await db
+    .insert(feeds)
+    .values({
+      name: feedName,
+      url,
+      userId,
+    })
+    .returning();
+
+  return firstOrUndefined(result);
+}
+
+export async function getFeeds() {
+  const result = await db.select().from(feeds);
+  return result;
+}
+
+export async function getFeedByURL(url: string) {
+  const result = await db.select().from(feeds).where(eq(feeds.url, url));
+  return firstOrUndefined(result);
+}
